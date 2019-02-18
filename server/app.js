c9c3518ee182976ce6517c880600e473fd720a90
Sergio Cinos <sergio.cinos@gmail.com>
2019-02-18 21:55:17 +1100

    Add eslint+prettier for code style checks

--- server/app.js
+++ server/app.js
@@ -1,42 +1,40 @@
-//MODULES
+// MODULES
 const express = require('express');
+const pg = require('pg');
 const setupMiddelware = require('./middleware/appMiddleware');
-const pg = require("pg");
-//MIDDLEWARE
+
+// MIDDLEWARE
 const app = express();
 setupMiddelware(app);
-require('dotenv').config()
+require('dotenv').config();
 
-
-//Setup Database
+// Setup Database
 const client = new pg.Client({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
-})
+});
 
-client.connect((err) => {
-  if(err) return console.log(err);
-  else console.log("Connected to dataabse")
+client.connect(err => {
+  if (err) return console.log(err);
+  return console.log('Connected to dataabse');
+});
 
-})
-
-app.get("/school", (req, res) => {
+app.get('/school', (req, res) => {
   try {
-    client.query("select * from school", (err, result) => {
-      if(err) {
+    client.query('select * from school', (err, result) => {
+      if (err) {
         console.log(err);
+      } else {
+        res.status(200).send(result.rows);
       }
-
-      else {
-        res.status(200).send(result.rows)
-      }
-    })
-  } catch(error) {console.log(error)}
-})
-
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 // create table school (
 //   school_id serial PRIMARY KEY,
@@ -46,16 +44,13 @@ app.get("/school", (req, res) => {
 //   students int
 // );
 
-
 // Test insert query for get request above ^
-// insert into school(name, address, postal_code, students) values ("Mission Hills", "San Marcos", 92069, 3000);
+//  insert into school(name, address, postal_code, students)
+//     values ("Mission Hills", "San Marcos", 92069, 3000);
 
-
-
-
-
-app.get("/", (req, res) => {res.status(200).send("Hello")})
-
+app.get('/', (req, res) => {
+  res.status(200).send('Hello');
+});
 
 app.use((req, res) => {
   res.status(404).json({
